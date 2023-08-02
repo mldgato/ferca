5391141be5b8865b2aebc187f4a7a34651a2c0bc
mldgato <manueldardonhotmail.com>
2023-08-02 09:59:16 -0600

    Actualización 02 de agosto 2023 0959

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,25 +29,15 @@ class DatabaseSeeder extends Seeder
             'password' => 'Ferc@23'
         ]);
 
-        \App\Models\Supplier::factory(10)->create();
+        /* \App\Models\Supplier::factory(10)->create(); */
         $this->call(WarehouseSeeder::class);
         $this->call(RackSeeder::class);
         \App\Models\Measure::factory(7)->create();
-        \App\Models\Product::factory(100)->create();
+        /* \App\Models\Product::factory(100)->create();
         \App\Models\Customer::factory(5)->create();
         \App\Models\Buy::factory(10)->create();
         \App\Models\Buydetail::factory(50)->create();
         \App\Models\Sale::factory(10)->create();
-        \App\Models\Saledetail::factory(50)->create();
-
-        /* 
-            UPDATE sales s
-                JOIN (
-                SELECT sale_id, SUM(quantity * price) AS total_pay
-                FROM saledetails
-                GROUP BY sale_id
-                ) sd ON s.id = sd.sale_id
-                SET s.pay = sd.total_pay;
-        */
+        \App\Models\Saledetail::factory(50)->create(); */
     }
 }
